c2a2211839b5890e2994fa5e48f783ea7fd65bd0
gskim <kiseon1987@gmail.com>
2022-07-11 23:01:24 +0900

    정규식 -> stack 방식으로 refactoring

--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -1,10 +1,9 @@
 (ns aoc2018_5
-  (:import (java.util.regex Pattern))
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s] [clojure.java.io :as io]))
 
-(def sample "resources/day5.txt")
-
-(def input (slurp sample))
+(defn get-input [] (-> "day5.txt"
+                       (io/resource)
+                       (slurp)))
 
 (defn char-range
   "시작 char 부터 끝 char 까지의 char 목록 return
@@ -20,9 +19,9 @@
    output #`(aA|Aa|bB|Bb|cC|Cc|dD|Dd|eE|Ee)`
    "
   [list]
-  (Pattern/compile (str "("
-                        (s/join "|" (reduce (fn [acc v] (conj acc (str v (s/upper-case v)) (str (s/upper-case v) v))) [] list))
-                        ")")))
+  (re-pattern (str "("
+                   (s/join "|" (reduce (fn [acc v] (conj acc (str v (s/upper-case v)) (str (s/upper-case v) v))) [] list))
+                   ")")))
 
 (defn get-regex-by-alphabet
   "전달받은 두 알파벳 범위의 대소문자 조합 정규식 return
@@ -41,10 +40,14 @@
 (defn remove-by-regex
   "전달받은 문자열을 전달받은 정규식으로 더이상 치환할게 없을때까지 치환후 최종결과값 return"
   [string regex]
-  (let [replaced-string (s/replace string regex "")]
-    (if (= string replaced-string)
+  (loop [replaced-string (s/replace string regex "")
+         before-string   string]
+    (if (= before-string replaced-string)
       replaced-string
-      (remove-by-regex replaced-string regex))))
+      (recur (s/replace replaced-string regex "") replaced-string))))
+
+(defn get-upper-case-char [c]
+  (char (- (int c) 32)))
 
 (defn remove-by-alphabet
   "문자열에있는 전달받은 alphabet 의 대소문자들을 지운다.
@@ -52,7 +55,7 @@
    output `bbcc`
    "
   [string alphabet]
-  (s/replace string (Pattern/compile (str alphabet "|" (s/upper-case alphabet))) ""))
+  (s/join "" (remove #{alphabet (get-upper-case-char alphabet)} string)))
 
 (defn part1 [input-string]
   (-> input-string
@@ -63,9 +66,36 @@
   (->> (char-range \a \z)
        (map #(remove-by-alphabet input-string %))
        (map part1)
-       (sort)
-       first))
+       (apply min)))
+
+(defn reactive-condition?
+  [char1 char2]
+  (and (not= char1 char2)
+       (= (s/upper-case char1)
+          (s/upper-case char2))))
+
+(defn reaction-polymer [polymer-string]
+  (reduce (fn [polymer char]
+            (if (seq polymer)
+              (if (reactive-condition? (peek polymer) char)
+                (pop polymer)
+                (conj polymer char))
+              (conj polymer char)))
+          [] polymer-string))
+
+(defn part1-refact [input-string]
+  (-> input-string
+      (reaction-polymer)
+      count))
+
+(defn part2-refact [input-string]
+  (->> (for [polymer (char-range \a \z)]
+         (part1-refact (remove-by-alphabet input-string polymer)))
+       (apply min)))
 
 (comment
-  (time (part1 input))
-  (time (part2 input)))
+  (time (part1 (get-input)))
+  (time (part2 (get-input)))
+  (time (part1-refact (get-input)))
+  (time (part2-refact (get-input))))
+
