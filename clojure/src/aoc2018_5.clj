e3d5413bbd5ca4fa801711e3f644307a7f910f6f
gskim <kiseon1987@gmail.com>
2022-07-11 12:57:09 +0900

    day5

--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -1,16 +1,81 @@
-(ns aoc2018_5)
-;; 파트 1
-;; 입력: dabAcCaCBAcCcaDA
+(ns aoc2018_5
+  (:import (java.util.regex Pattern))
+  (:require [clojure.string :as s]))
 
-;; 같은 종류의 소문자와 대문자는 서로 ‘반응‘하여 사라짐. aABb -> ‘’
-;; 사라진 자리는 진공이 되기 때문에 다른 문자들이 붙게 되고, 또 그 문자들끼리 반응할 수 있음.  abBA-> aA -> ‘’
-;; 바로 옆에 붙어있어야만 서로 반응함. abAB -> abAB (반응 없음)
-;; 대문자-대문자, 소문자-소문자는 서로 반응하지 않음. aabAAB-> aabAAB (반응 없음)
-;; 예시 dabAcCaCBAcCcaDA => dabCBAcaDA
+(def sample "resources/day5.txt")
 
-;; 주어진 input 에서 최종으로 남는 문자열을 리턴하시오.
+(def input (slurp sample))
 
-;; 파트 2
-;; 주어진 문자열에서 한 유닛 (대문자와 소문자)을 전부 없앤 후 반응시켰을 때, 가장 짧은 문자열의 길이를 리턴하시오.
-;; 예를 들어 dabAcCaCBAcCcaDA 에서 a/A를 없애고 모두 반응시키면 dbCBcD가 되고 길이는 6인데 비해,
-;; 같은 문자열에서 c/C를 없애고 모두 반응시키면 daDA가 남고 길이가 4이므로 4가 가장 짧은 길이가 됨.
+(def alphabet-list [\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z])
+
+(defn char-range
+  "시작 char 부터 끝 char 까지의 char 목록 return
+   input a c
+   output (a b c)
+   "
+  [start end]
+  (map char (range (int start) (inc (int end)))))
+
+(defn make-regex-string
+  "전달받은 알파벳들로 (소문자대문자|대문자소문자) 정규식 표현을 return
+   input (a b c d e)
+   output #`(aA|Aa|bB|Bb|cC|Cc|dD|Dd|eE|Ee)`
+   "
+  [list]
+  (Pattern/compile (str "("
+                        (s/join "|" (reduce (fn [acc v] (conj acc (str v (s/upper-case v)) (str (s/upper-case v) v))) [] list))
+                        ")")))
+
+(defn get-regex-string
+  "전달받은 두 알파벳 범위의 대소문자 조합 정규식 return
+   알파벳 하나만 전달시 해당 알파벳으로만 조합하여 reutrn
+   input `a` `c`
+   output #`(aA|Aa|bB|Bb|cC|Cc)`
+   input `a`
+   output #`(aA|Aa)`
+   "
+  ([alphabet]
+   (get-regex-string alphabet alphabet))
+  ([start-alphabet end-alphabet]
+   (let [list (char-range start-alphabet end-alphabet)]
+     (make-regex-string list))))
+
+(defn remove-by-regex
+  "전달받은 문자열을 전달받은 정규식으로 더이상 치환할게 없을때까지 치환후 최종결과값 return"
+  [string regex]
+  (let [replaced-string (s/replace string regex "")]
+    (if (= string replaced-string)
+      replaced-string
+      (remove-by-regex replaced-string regex))))
+
+(defn remove-by-alphabet
+  "문자열에있는 전달받은 alphabet 의 대소문자들을 지운다.
+   input `ababcc` `a`
+   output `bbcc`
+   "
+  [string alphabet]
+  (s/replace string (Pattern/compile (str alphabet "|" (s/upper-case alphabet))) ""))
+
+(defn part1 [input-string]
+  (-> input-string
+      (remove-by-regex (get-regex-string \a \z))
+      count))
+
+(defn part2 [input-string]
+  (->> alphabet-list
+       (map #(remove-by-alphabet input-string %))
+       (map part1)
+       (sort)
+       first))
+
+(defn samplef [n]
+  (println (str "starting " n))
+  (Thread/sleep 3000)
+  n)
+
+(comment
+  (doall (pmap samplef (range 0 100))))
+
+(comment
+  (time (part1 input))
+  (time (part2 input)))
