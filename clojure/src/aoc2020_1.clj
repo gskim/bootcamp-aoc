b0e905dad4938263996ffdc23c42b958e25b7222
gskim <kiseon1987@gmail.com>
2022-07-21 14:29:58 +0900

    2020 day1 refactoring

--- clojure/src/aoc2020_1.clj
+++ clojure/src/aoc2020_1.clj
@@ -6,59 +6,38 @@
   (s/split-lines input))
 
 (defn get-input []
-  (-> "2020_day1.txt"
-      io/resource
-      slurp
-      s/split-lines))
+  (->> "2020_day1.sample.txt"
+       io/resource
+       slurp
+       s/split-lines
+       (map #(Integer/parseInt %))))
 
 (defn make-two-pair [input]
-  (let [pairs   (for [x input
-                      y input]
-                  [x y])]
-    (filter (fn [[x y]] (not= x y)) pairs)))
+  (for [x     input
+        y     input
+        :when (= (+ x y) 2020)]
+    [x y]))
 
 (defn make-three-pair [input]
-  (let [pairs   (for [x input
-                      y input
-                      z input]
-                  [x y z])]
-    (filter (fn [[x y z]] (and (not= x y) (not= x z) (not= y z) (not= x y z))) pairs)))
+  (for [x     input
+        y     input
+        z     input
+        :when (= (+ x y z) 2020)]
+    [x y z]))
 
-(defn init-state [data]
-  {:data          data
-   :current-index 0})
-
-(defn increse-current-index [state]
-  (update state :current-index inc))
-
-(defn is-not-sum-2020? [{:keys [data current-index]}]
-  (let [current-data (nth data current-index)]
-    (not= 2020 (apply + current-data))))
-
-(defn get-sum-2020 [state]
-  (->> state
-       (iterate increse-current-index)
-       (drop-while is-not-sum-2020?)
-       first))
-
-(defn multiplication [{:keys [data current-index]}]
-  (let [target (nth data current-index)]
-    (apply * target)))
+(defn multiplication [data]
+  (apply * data))
 
 (defn part1 []
   (->> (get-input)
-       (map #(Integer/parseInt %))
        make-two-pair
-       init-state
-       get-sum-2020
+       first
        multiplication))
 
 (defn part2 []
   (->> (get-input)
-       (map #(Integer/parseInt %))
        make-three-pair
-       init-state
-       get-sum-2020
+       first
        multiplication))
 
 (comment
